7f4a60a0547ba48b47296274a1f86c9610b0a701
Nikolay Smolov <n.smolov@yandex.ru>
2022-09-29 23:05:53 +0300

    adds types for ingredient-thumbnail

--- src/components/ingredient-thumbnail/ingredient-thumbnail.tsx
+++ src/components/ingredient-thumbnail/ingredient-thumbnail.tsx
@@ -1,15 +1,14 @@
 import style from './ingredient-thumbnail.module.css';
-import PropTypes from 'prop-types';
 
-export const IngredientThumbnail = ({ image, more = null }) => {
+interface IIngredientThumbnail {
+  image: string;
+  more?: number | null;
+}
+
+export const IngredientThumbnail = ({ image, more = null }: IIngredientThumbnail) => {
   return (
     <span className={style.thumbnail} style={{ backgroundImage: `url(${image})` }}>
       {more ? <p className={`${style.more} text text_type_main-default`}>+{more}</p> : null}
     </span>
   );
-};
-
-IngredientThumbnail.propTypes = {
-  image: PropTypes.string.isRequired,
-  more: PropTypes.number,
 };
